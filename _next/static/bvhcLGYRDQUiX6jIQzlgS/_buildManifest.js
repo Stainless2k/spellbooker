62094b1c74e2cc840c06e4b4e4591d6c44cbfb5d
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-11-29 00:22:18 +1100

    Updates

---
+++ _next/static/bvhcLGYRDQUiX6jIQzlgS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-8619d3a1a46e5850.js","static/chunks/917-3e65e40e7c27c9b7.js","static/chunks/pages/index-2d28a2b79a53fae7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
